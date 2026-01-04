dd2d9f32d4f15e13f5c27d89812d79bd83ffb43f
Oliunekits <orestlisuk@gmail.com>
2026-01-04 23:05:58 +0200

    Update yt_auto_split_upload.py

--- yt_auto_split_upload.py
+++ yt_auto_split_upload.py
@@ -21,7 +21,7 @@ SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
 MAX_UPLOADS_PER_RUN = 10
 UPLOADED_LIST_FILE = "uploaded.txt"
 
-DEFAULT_TAGS = ["shorts", "youtubeshorts", "viral", "trending", "shortsvideo"]
+DEFAULT_TAGS = ["shorts", "youtubeshorts", "viral", "trending", "shortsvideo"] # type any tag what you want
 DEFAULT_PRIVACY = "public"  
 
 
